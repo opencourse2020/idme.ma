f1d592a4d8c69487ad89a29247454c367a79c7d9
opencourse2020 <akaaboune@gmail.com>
2024-12-26 00:04:01 +0100

    OTP and 2FA

--- config/settings/base.py
+++ config/settings/base.py
@@ -245,8 +245,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'idme.ma@gmail.com'
-EMAIL_HOST_PASSWORD = '2XTr&GfzJD9cXD6'
+EMAIL_HOST_USER = env.str("DJANGO_EMAIL")
+EMAIL_HOST_PASSWORD = env.str("DJANGO_EMAIL_PASS")
 
 
 
