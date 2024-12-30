1db9967abb73c1646c83748bc0b5b94e03cdb7cf
opencourse2020 <akaaboune@gmail.com>
2024-12-30 12:21:51 +0100

    OTP and 2FA

--- config/settings/base.py
+++ config/settings/base.py
@@ -104,7 +104,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'user_language_middleware.UserLanguageMiddleware',
     "allauth.account.middleware.AccountMiddleware",
-    "defender.middleware.FailedLoginMiddlewares",
+    "defender.middlewares.FailedLoginMiddlewares",
 
 ]
 
