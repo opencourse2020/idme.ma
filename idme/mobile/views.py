01438a8f8faae25a52041c684f9fefa01e37e36d
opencourse2020 <akaaboune@gmail.com>
2024-12-31 21:09:51 +0100

    OTP and 2FA

--- idme/mobile/views.py
+++ idme/mobile/views.py
@@ -9,7 +9,12 @@ from django.views.generic import (
     TemplateView,
     View,
 )
+from guardian.mixins import LoginRequiredMixin
 from idme.profiles.models import Admin, User
 from idme.api import models
 from idme.api.views import IDVerifyView, FileUpdateView
 # Create your views here.
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "../templates/conditions.html"
